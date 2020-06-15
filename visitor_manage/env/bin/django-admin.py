f8d0bd569e1df55820e302af2c909b13b3b70b2d
parth-27 <parth1020patel@gmail.com>
2020-06-15 19:35:24 +0530

    Initial Commit

--- visitor_manage/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/pal/Desktop/Visitor-Management-System/visitor_manage/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
