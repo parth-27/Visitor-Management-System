51f031e25a003e5755ce9e85342c0f86da7983ee
dipikapawar <dipikapawar2001@gmail.com>
2020-06-15 21:16:42 +0530

    front screen added

---
+++ visitor_manage/src/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+from .views import *
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('user',views.userLogin,name='userLogin'),
+]
