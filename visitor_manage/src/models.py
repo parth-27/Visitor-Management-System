ac2db7eacb93edfb5e11c59d14bd69738c3e3c3b
dipikapawar <dipikapawar2001@gmail.com>
2020-06-16 13:37:54 +0530

    database table-1 added

--- visitor_manage/src/models.py
+++ visitor_manage/src/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+GENDER= [
+    ('male', 'Male'),
+    ('female', 'Female'),
+    ('other', 'Other'),
+    ]
+class user(models.Model):
+    username=models.CharField(max_length=50, unique=True)
+    name=models.CharField(max_length=100)
+    password=models.CharField(max_length=12)
+    mail=models.CharField(max_length=100)
+    contact=models.IntegerField()
+    gender=models.CharField(max_length=10 ,choices=GENDER)
+    photo=models.ImageField()
