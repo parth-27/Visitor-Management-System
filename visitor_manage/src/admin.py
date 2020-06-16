cf581b334d98fa99717f30d6b2c82bf88fbb33da
parth-27 <parth1020patel@gmail.com>
2020-06-16 18:28:35 +0530

    Database Tables

--- visitor_manage/src/admin.py
+++ visitor_manage/src/admin.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 from .models import *
-from import_export.admin import ImportExportActionModelAdmin
-# Register your models here.
-@admin.register(User)
-class ViewAdmin(ImportExportActionModelAdmin):
-    pass
+
+admin.site.register(SuperAdmin)
+admin.site.register(User)
+admin.site.register(Admin)
+admin.site.register(Visitor)
--- visitor_manage/src/models.py
+++ visitor_manage/src/models.py
@@ -1,19 +1,57 @@
 from django.db import models
 
 # Create your models here.
-GENDER= [
+GENDER = [
     ('male', 'Male'),
     ('female', 'Female'),
     ('other', 'Other'),
-    ]
-class User(models.Model):
-    username=models.CharField(max_length=50, unique=True)
-    name=models.CharField(max_length=100)
-    password=models.CharField(max_length=12)
-    mail=models.CharField(max_length=100)
-    contact=models.IntegerField()
-    gender=models.CharField(max_length=10 ,choices=GENDER)
-    photo=models.ImageField()
+]
+
+VISITING_HOURS = [
+    (1),
+    (2),
+    (3),
+    ("More than 3"),
+]
+
+
+class SuperAdmin(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=20)
 
     def __str__(self):
         return self.username
+
+
+class Admin(models.Model):
+    gate = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    mail = models.EmailField(unique=True)
+    contact = models.IntegerField()
+    gender = models.CharField(max_length=10, choices=GENDER)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    mail = models.EmailField(unique=True)
+    contact = models.IntegerField()
+    gender = models.CharField(max_length=10, choices=GENDER)
+    photo = models.ImageField()
+    bdate = models.DateField()
+
+    def __str__(self):
+        return self.username
+
+
+class Visitor(models.Model):
+    gateId = models.ForeignKey(Admin, default=1, on_delete=models.SET_DEFAULT)
+    userId = models.ForeignKey(User, default=1, on_delete=models.SET_DEFAULT)
+    visitDate = models.DateField()
+    checkin = models.DateTimeField()
+    checkout = models.DateTimeField()
+    feedback = models.TextField()
+    reason = models.TextField(max_length=1000)
