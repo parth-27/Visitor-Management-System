30029f2d58dafa7d3303da6d99625f113eb22c97
dipikapawar <dipikapawar2001@gmail.com>
2020-06-19 21:00:02 +0530

    edit admin and delete admin added, on spot gate pass done

--- visitor_manage/visitor_manage/urls.py
+++ visitor_manage/visitor_manage/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import settings
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('src.urls')),
-]
-
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
